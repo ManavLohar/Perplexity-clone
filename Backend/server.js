40fb74709afb4b917428203f700caeec540bbdef
ManavLohar <manavlohar414141@gmail.com>
2026-05-10 17:27:22 +0530

    Frontend:-
    1.) Implement Redux for state management.
    2.) Connect socket io with backend
    
    Backend:-
    
    1.) Implement Socket.io service using httpServer.
    
    Signed-off-by: ManavLohar <manavlohar414141@gmail.com>

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,18 @@
 import "dotenv/config";
 import app from "./src/app.js";
 import { connectDB } from "./src/config/db.js";
+import { createServer } from "http";
+import { initSocket } from "./src/sockets/server.socket.js";
 
 const PORT = process.env.PORT;
+const httpServer = createServer(app);
+initSocket(httpServer);
 
 connectDB().catch((err) => {
   console.error("MongoDB connection error: ", err);
   process.exit(1);
 });
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log("Server started on port", PORT);
 });
--- Frontend/src/features/chat/pages/Dashboard.jsx
+++ Frontend/src/features/chat/pages/Dashboard.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
+import { useChat } from "../hooks/useChat";
 
 const Dashboard = () => {
   const { user } = useSelector((state) => state.auth);
-  console.log(user);
+  const { initializeSocketConnection } = useChat();
+
+  useEffect(() => {
+    initializeSocketConnection();
+  }, []);
+
   return <div>Dashboard</div>;
 };
 
