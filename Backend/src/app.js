8592f5a6ff6356f42b70824e31c13ad8316a4c1c
Manav Lohar <manavlohar414141@gmail.com>
2026-06-13 01:01:30 +0530

    Setup dist file in backend side

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,18 +3,31 @@ import express from "express";
 import authRouter from "./routes/auth.routes.js";
 import cors from "cors";
 import chatRouter from "./routes/chat.routes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
   }),
 );
+
 app.use("/api/auth", authRouter);
 app.use("/api/chats", chatRouter);
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("/{*splat}", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 export default app;
